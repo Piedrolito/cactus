a276a5a4ea89d6f56081157d37f3fa2651fcd34a
amorim <pierre.amorim@etu.univ-paris-diderot.fr>
2022-09-15 17:27:32 +0200

    Test

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const { Console } = require('console');
 const express = require('express');
 
+
 const app = express();
+
+app.use(express.static('public'));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -11,32 +14,8 @@ app.use((req, res, next) => {
   });
 
 app.get('/', (req, res, next) => {
-
-    res.sendFile('main.html', {root: __dirname });
-    next();
-
+    res.sendFile(__dirname + '/main.html');
 });
 
-app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
-  });
 
 module.exports = app;
